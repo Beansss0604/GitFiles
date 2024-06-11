35636284eb7afefd8b03d10cf9b6453c18443c15
Beansss0604 <154306865+Beansss0604@users.noreply.github.com>
2024-06-11 20:04:10 +0800

    Add files via upload

---
+++ proj_functions.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <windows.h>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
+
+void gotoxy(int x, int y);
+void desReminder();
+void design();
+void Login();
+void desLogin();
+void desRegistration();
+void Register();
+void desForgot();
+void forgotPassword();
+void accFound();
+string getPassword();
+int MainMenu();
